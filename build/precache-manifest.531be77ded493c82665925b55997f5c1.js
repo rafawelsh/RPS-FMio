db79c4ec77eb9c589b4f43fa9034a8f506334776
rafawelsh <mrrafael1912@gmail.com>
2020-11-23 19:49:33 -0800

    Live dome ready

---
+++ build/precache-manifest.531be77ded493c82665925b55997f5c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a934e877e187887005e9ba258e7df987",
+    "url": "/RPS-FMio/index.html"
+  },
+  {
+    "revision": "2ed436812e09da490f04",
+    "url": "/RPS-FMio/static/css/main.ece3cd5f.chunk.css"
+  },
+  {
+    "revision": "9ca70e4bdcb9b56ff60e",
+    "url": "/RPS-FMio/static/js/2.1d73bb1b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RPS-FMio/static/js/2.1d73bb1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ed436812e09da490f04",
+    "url": "/RPS-FMio/static/js/main.14c5c26b.chunk.js"
+  },
+  {
+    "revision": "4fc81c765b039ba62051",
+    "url": "/RPS-FMio/static/js/runtime-main.81c31eea.js"
+  }
+]);
